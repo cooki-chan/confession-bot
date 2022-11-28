e4cacf2bd472b7e42eb34ef6041d36ecde960696
Ethan Nguyen <72627687+cooki-chan@users.noreply.github.com>
2022-11-28 14:37:34 -0500

    Comments + Updated to production code
    Updated the code to the version used in production, as well as added comments for readability. Will do a rewrite soon.

--- confess.py
+++ confess.py
@@ -1,82 +1,86 @@
-import discord
+import discord as discord
 from discord.ext import commands
 import random
 import json
+import os
+import threading
 
-client = commands.Bot(command_prefix='!', help_command=None)
-outputID = 885406314749112351 #venting channel, or output
-responseID = 933850247979995136 #venting-response, not currently used.
-cookie = 418160770321350667 #our lord and savior
+intents = discord.Intents.default()
+intents.message_content = True
 
-words = ["want to die", "kill myself", "hate myself", "suicide", "not exist", "dont matter", "don't matter", "don't want to be alive", "dont want to be alive", "cutting", "noose", "gun", "revolver"] #words that trigger suicide response
-spam_words = ["add ur own words"] # anti-spam shit cuz apparently i need that
-nme = []
-with open('names.json') as _file:
-    file = json.load(_file)
-    nme = file
+client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
+outputID = -1 #Channel to send messages (Channel ID)
+cookie = -1 #Manual Review User (User ID) 
+#-1 IS A PLACEHOLDER
 
-support = "If you are about to commit suicide, please don't. Suicide is not painless. It will hurt. Alot.\nIt is a permanent solution to a temporary problem. \n\nSuicide Hotline: +1-800-273-8255\nImpartial Facts about Suicide: lostallhope.com\nSuicide Chatting Hotline: iamalive.org, crisistextline.org" #suicide message
-dumbass = "One of the words in your message contains words that may be off topic. If this is a mistake, please message the bot with the tag ->sus at the start of your message, and cookie will manualy send the message. The message will be completely anonomous." #anti-spam filter message
+words = [] #Suicide Message Trigger (Sends support)
+spam_words = [] #Negative Word Filter (Sends dumbass)
+
+support = "If you are about to commit suicide, please don't. Suicide is not painless. It will hurt. Alot.\nIt is a permanent solution to a temporary problem. \n\nSuicide Hotline: +1-800-273-8255\nImpartial Facts about Suicide: lostallhope.com\nSuicide Chatting Hotline: iamalive.org, crisistextline.org"
+dumbass = "One of the words in your message contains words that may be off topic. If this is a mistake, please message the bot with the tag ->sus at the start of your message, and cookie will manualy send the message. The message will be completely anonomous."
 
 @client.event
 @commands.dm_only()
 async def on_message(message):
     user = message.author
-    name = nme[random.randint(0, len(nme) - 1)] + ": "
+    name = ""
     msg = message.content
-
-    if msg.lower().startswith("HIDDEN COMMAND"): #announcement command, hidden so u guys cant impersonate me
-        name = "**ANNOUNCEMENT**: "
-        msg = msg.replace("HIDDEN COMMAND ", "", 1)
 
     output = client.get_channel(outputID)
 
     if message.author == client.user:
         return   
 
-
     if isinstance(message.channel, discord.channel.DMChannel):
         skip = False
         dumdum = False
         sad = False
-        for i in words: 
-            if i in msg.lower() and user.id != cookie: #suicide trigger
+        
+        for i in words: #Suicidal Message Filter
+            if i in msg.lower() and user.id != cookie:
                sad = True
 
-        for i in spam_words:
-            if i in msg.lower() and user.id != cookie: #anti-spam trigger
+        for i in spam_words: #Filtered Word Filter
+            if i in msg.lower() and user.id != cookie:
                 dumdum = True
                 skip = True
 
-        if msg.lower().startswith("->sus"): #message sent to me for review
+        if msg.lower().startswith("->sus"): #Manual Review Trigger
             skip = True
             sad = False
             dumdum = False
             await client.get_user(cookie).send(msg)
             
+        if skip == False:
+            if msg != ""   :
+                embedVar = discord.Embed(title="", description="", color=0x00ff00)
+                if msg.lower().startswith("ADD A PREFIX HERE"): #Used for owner messages! Make sure to change!
+                    msg = msg.replace("PREFIX HERE TOO!!! ", "", 1)
+                    embedVar.add_field(name="**SYSTEM**:", value=msg, inline=False)
+                else:
+                    if len(msg) < 1024: #If the message is too long to fit in an embed, send as raw text
+                        embedVar.add_field(name="Message:", value=msg, inline=False)
+                        await output.send(embed=embedVar)
+                    else:
+                        await output.send(msg)
 
-        if skip == False: # sending msg check 
-            await output.send(name + msg) # send message
-            if message.attachments != []: # attachment check
-                for i in message.attachments: # send all atachments
+            if message.attachments != []: #Send Attatchments (Words in images bypasses filters, be careful.) Images can also be much more triggering, so remove this block if you don't want image support
+                for i in message.attachments:
                     await output.send(i.url)
 
-        if dumdum == True: # anti-spam message
+        if dumdum == True: #Send dumbass message
             await user.send(dumbass)
 
-        if sad == True: # suicide message
+        if sad == True: #Send Support message
             await user.send(support) 
             
-
-
-    if message.channel.id == outputID: # no messages in venting
+    if message.channel.id == outputID: #If a message is sent in the channel the bot talks in, delete (Used to keep convos on topic most of the time)
         await message.delete()
         await user.send("Please do not send messages into venting. If you are attempting to vent, please message me instead. If responding to a vent, please send it into venting-response. This is for anti-off-topic reasons. Your message was: " + msg)
 
 @client.event
-async def on_ready(): #check if bot runs or not
+async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     activity = discord.Activity(name="DM me, and i'll send a message to venting anonymously", type=discord.ActivityType.playing)
     await client.change_presence(activity=activity)
-
-client.run('KEY')
+client.run('CLIENT KEY')
